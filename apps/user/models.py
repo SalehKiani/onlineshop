b7b8ed97eec9393d70aaf87dfe34f16f57417354
SalehKiani <kiani.mohammadsaleh@gmail.com>
2024-09-03 23:34:32 +0330

    sparkles:feat: change project structure and user model and add OTP.

---
+++ apps/user/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from apps.utils.OTP import generate_otp
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email' or 'phone_number']
+
+    def __str__(self):
+        return self.username
+
+
+
+class OTP(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='otps', on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6, default=generate_otp)
+    expiry = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"OTP for {self.user.username}"
